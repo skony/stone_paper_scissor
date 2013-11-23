35ee48594cbdc4cdc0c20b1692c5867e36003a94
skony <piotr.skon@gmail.com>
2013-11-23 13:56:51 +0100

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+	img = cv2.imread('DSC00562.jpg')
+	imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	ret,thresh = cv2.threshold(imgray,127,255,0)
+	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	drawing = np.zeros(img.shape,np.uint8)
+	for cnt in contours:
+		color = np.random.randint(0,255,(3)).tolist()
+		cv2.drawContours(drawing,[cnt],0,color,2)	
+		cv2.imshow('output',drawing)
