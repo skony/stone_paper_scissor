6b3e378ce5ef268702f9922101a2338a02838ea0
skony <piotr.skon@gmail.com>
2013-12-04 12:55:52 +0100

    dwa kwafraty na pałe

--- FindHand2.py
+++ FindHand2.py
@@ -9,22 +9,31 @@ if __name__ == '__main__':
 	hsvt = cv2.cvtColor(target,cv2.COLOR_BGR2HSV)
 
 	#mix Channels
-	hue = np.empty(hsv.shape, np.uint8)
-	cv2.mixChannels(hsv,hue, [0, 0])	
+	hue = np.empty(hsv.shape, np.float32)
+	cv2.mixChannels(hsv.astype('float32'),hue, [0, 0])	
 	 
 	# calculating object histogram
-	roihist = cv2.calcHist([hue],[0, 1], None, [180, 256], [0, 180, 0, 256] )
+	roihist = cv2.calcHist([hue],[0, 0], None, [180, 256], [0, 180, 0, 256] )
 	 
 	# normalize histogram and apply backprojection
 	cv2.normalize(roihist,roihist,0,255,cv2.NORM_MINMAX)
-	dst = cv2.calcBackProject([hsvt],[0,1],roihist,[0,180,0,256],1)
+	dst = cv2.calcBackProject([hsvt],[0,0],roihist,[0,180,0,256],1)
+
+	#a minsziftem
+	term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+	ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+
+	# Draw it on image
+	x,y,w,h = track_window
+	img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+	cv2.imshow('img2',img2)
 	 
 	# Now convolute with circular disc
 	disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 	cv2.filter2D(dst,-1,disc,dst)
 	 
 	# threshold and binary AND
-	ret,thresh = cv2.threshold(dst,50,255,0)
+	#ret,thresh = cv2.threshold(dst,0,255,0)
 	thresh = cv2.merge((thresh,thresh,thresh))
 	res = cv2.bitwise_and(target,thresh)
 	 
