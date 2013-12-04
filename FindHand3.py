8d6d766d7b3c0aae3d786e53739c3cdd471df2fe
skony <piotr.skon@gmail.com>
2013-12-04 19:54:22 +0100

    jako tako dziala

--- FindHand3.py
+++ FindHand3.py
@@ -1,53 +1,46 @@
 import cv2
 import numpy as np
+from contours import contour_thingy, winner 
 
 if __name__ == '__main__':
 	
- 
-	#roi = cv2.imread('hope.jpg')
-	#hsv = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
-	
+	s1, s2 = 0, 0
 	cam=cv2.VideoCapture(0)
 	cam.open
 	ret,frame = cam.read()
-	
-	# setup initial location of window
-	#r,h,c,w = 250,90,400,125  # simply hardcoded the values
-	r,h,c,w = 480,-240,0,240
+	tick = 10
+	T, t1, t2 = 0, 0, 0
+	r,h,c,w = 240,240,0,240
 	track_window = (c,r,w,h)
+	y2,h2,x2,w2 = 240,240,400,240
 
-	y2,h2,x2,w2 = 480,-240,640,-240
-
-	#mix Channel
-	#hue = np.empty(hsv.shape, np.float32)
-	#cv2.mixChannels(hsv.astype('float32'),hue, [0, 0])	
-	 
-	# calculating object histogram
-	#roihist = cv2.calcHist([hue],[0, 0], None, [180, 256], [0, 180, 0, 256] )
-	 
-	# normalize histogram and apply backprojection
-	#cv2.normalize(roihist,roihist,0,255,cv2.NORM_MINMAX)
-
-	#a minsziftem
-	#tem_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
-	
 	while(1):
 		ret ,frame = cam.read()
 
 		if ret == True:
-			#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-			#dst = cv2.calcBackProject([hsv],[0,0],roihist,[0,180,0,256],1)
-
-			# apply meanshift to get the new location
-			#ret, track_window = cv2.meanShift(dst, track_window, term_crit)
-
- 			# Draw it on image
  			x,y,w,h = track_window
+
+			if T > 2 and tick==0:
+				handL = frame[x:x+w,y:y+h]
+				handR = frame[x2:x2+w2, y2:y2+h2]
+				p1 = contour_thingy(handL)
+				p2 = contour_thingy(handR)
+				s1, s2 = winner(p1, s1, p2, s2)
+				tick = 10
+				T = 0
+			elif T > 2:
+				cv2.putText(frame, str(tick), (280,100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255) )
+				t1 = cv2.getTickCount()
+				tick = tick-1
+				T = 0
+
 			img2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
 			img3 = cv2.rectangle(frame, (x2,y2), (x2+w2,y2+h2), (0,255),2)
-			cv2.imshow('img2',frame )
-
+			cv2.putText(frame, str(s1), (10,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255) )
+			cv2.putText(frame, str(s1), (480,200), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255) )
+			cv2.imshow('img2', frame)
 			k = cv2.waitKey(60) & 0xff
+
 			if k == 27:
 				break
 			else:
@@ -55,6 +48,9 @@ if __name__ == '__main__':
 
 		else:
 			break
+		t2 = cv2.getTickCount()
+		T += (t2 - t1)/cv2.getTickFrequency()
+		print T
 
 	cv2.destroyAllWindows()
 	cam.release() 
